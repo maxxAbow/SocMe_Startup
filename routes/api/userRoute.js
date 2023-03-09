f16ddcc495589b0c1256c875e025e8f13eaea0da
maxxAbow <mbowman@welland.com>
2023-03-09 18:41:34 -0500

    next step, seed

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
+const deleteThought = require('./thoughtRoute');
 const {
     getSingleUser,
     getUsers,
     createUser,
+    deleteUser,
 } = require('../../controller/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser);
+
+router.route('/:userId').delete(deleteUser);
+
+if (!userId) {
+    router.route('/:userId').delete(deleteThought);
+};
 
 module.exports = router;
